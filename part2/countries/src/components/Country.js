d63ff3c5541d23d3858131369460c9631028bb0a
jcLucio <c.lucio3@gmail.com>
2021-05-28 13:42:23 -0500

    Add countries exercise (2.13)

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,5 +1,20 @@
+import React, { useState } from 'react';
+import CountryDetails from './CountryDetails';
+
 const Country = ({ country }) => {
-  return <p>{country.name}</p>;
+  const [show, setShow] = useState(false);
+
+  const toggleShow = () => setShow(!show);
+
+  return (
+    <div>
+      <p>
+        {country.name}{' '}
+        <button onClick={toggleShow}>{show ? 'hide' : 'show'}</button>
+      </p>
+      {show && <CountryDetails country={country} />}
+    </div>
+  );
 };
 
 export default Country;
